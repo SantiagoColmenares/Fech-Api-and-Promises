6ce9ba12eeeab33f83b084e2bac5fd1685ed6849
SantiagoColmenaresCampus <santiagocolmeasmc0423@gmail.com>
2023-04-27 07:36:55 -0500

    Se añaden dos filtros mas owner and hero

--- js/promise.js
+++ js/promise.js
@@ -1,4 +1,6 @@
 import { getHeroeById } from './index.js';
+import { getHeroeByOwner } from './index.js';
+import { getHeroeByName } from './index.js';
 
 const getHeroeByIdAsync = ( id ) => {
     return new Promise( (resolve, reject) =>{
@@ -8,11 +10,47 @@ const getHeroeByIdAsync = ( id ) => {
             if ( p1) {
                 resolve( p1 );
             } else {
-                reject( 'No se pudo encontrar el heroe' );
+                reject( 'No se pudieron encontrar los heroes.' );
             }
         }, 2000)
     })
 }
+
+const getHeroeByOwnerAsync = (owner) => {
+    return new Promise( (resolve, reject) =>{
+
+        setTimeout( () => {
+            const o1 = getHeroeByOwner( owner );
+            if ( o1 ) {
+                resolve( o1 );
+            } else {
+                reject( 'No se pudieron encontrar los heroes.')
+            }
+        }, 2000)
+    })
+}
+
+const getHeroeByNameAsync = (name) => {
+    return new Promise((resolve, reject) => {
+        
+        setTimeout( () => {
+            const n1 = getHeroeByName ( name );
+            if ( n1 ) {
+                resolve ( n1 );
+            } else {
+                reject( 'No se pudieron encontrar los heroes.')
+            }
+        }, 2000)
+    })
+}
+
+getHeroeByNameAsync('Batman')
+    .then( console.log)
+    .catch( console.warn );
+
+getHeroeByOwnerAsync('Marvel')
+    .then( console.log )
+    .catch( console.warn);
 
 getHeroeByIdAsync(3)
     .then( console.log )
